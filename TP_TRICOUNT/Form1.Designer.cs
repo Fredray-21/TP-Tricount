57aaf9f15ab1ac4d257cad588a955754cab57eb3
Frédéric Dabadie <dabadie.frederic@gmail.com>
2021-11-15 09:37:03 +0100

    edit form1

--- TP_TRICOUNT/Form1.Designer.cs
+++ TP_TRICOUNT/Form1.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabDepense = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabDepense);
+            this.tabControl.Controls.Add(this.tabPage2);
+            this.tabControl.Location = new System.Drawing.Point(12, 12);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(777, 497);
+            this.tabControl.TabIndex = 0;
+            // 
+            // tabDepense
+            // 
+            this.tabDepense.Location = new System.Drawing.Point(4, 26);
+            this.tabDepense.Name = "tabDepense";
+            this.tabDepense.Padding = new System.Windows.Forms.Padding(3);
+            this.tabDepense.Size = new System.Drawing.Size(769, 467);
+            this.tabDepense.TabIndex = 0;
+            this.tabDepense.Text = "DÉPENSES";
+            this.tabDepense.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 26);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(769, 467);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(801, 521);
+            this.Controls.Add(this.tabControl);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private TabControl tabControl;
+        private TabPage tabDepense;
+        private TabPage tabPage2;
     }
 }
