b648c3a70cfe0416986327c5761876b2035a944e
Frédéric Dabadie <dabadie.frederic@gmail.com>
2021-11-19 13:01:54 +0100

    add Exemple Icomparble

---
+++ TP_TRICOUNT/Properties/TriParticipant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_TRICOUNT.Properties
+{
+    internal class TriParticipant : IComparer<Participant>
+    {
+        public int Compare(Participant? x, Participant? y)
+        {
+            return x.GetBalance().CompareTo(x.GetBalance());
+        }
+    }
+}
