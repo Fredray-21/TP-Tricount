095f2dc599da5594eb277e82b043c5b302afe347
Frédéric Dabadie <dabadie.frederic@gmail.com>
2021-11-21 03:59:00 +0100

    Ajout new BDD

--- tp_tricount.sql
+++ tp_tricount.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost
--- Généré le :  lun. 15 nov. 2021 à 08:20
+-- Hôte : 127.0.0.1:3306
+-- Généré le : Dim 21 nov. 2021 à 02:57
 -- Version du serveur :  5.7.31
--- Version de PHP :  7.3.11
+-- Version de PHP : 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,47 +18,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `tp_tricount`
+-- Base de données : `tp_tricount`
 --
-
--- --------------------------------------------------------
-
---
--- Structure de la table `concerner`
---
-
-CREATE TABLE `concerner` (
-  `id_concerne` int(4) NOT NULL,
-  `id_depense` int(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `depense`
---
-
-CREATE TABLE `depense` (
-  `id` int(4) NOT NULL,
-  `intutile` varchar(30) NOT NULL,
-  `montant` float NOT NULL,
-  `date` date NOT NULL,
-  `id_payeur` int(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `membre`
---
-
-CREATE TABLE `membre` (
-  `id` int(4) NOT NULL,
-  `nom` varchar(30) NOT NULL,
-  `balance` float NOT NULL,
-  `monCoutTotal` float NOT NULL,
-  `id_tricount` int(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -67,86 +27,12 @@ CREATE TABLE `membre` (
 -- Structure de la table `tricount`
 --
 
-CREATE TABLE `tricount` (
-  `id` int(4) NOT NULL,
-  `nom` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `concerner`
---
-ALTER TABLE `concerner`
-  ADD PRIMARY KEY (`id_concerne`,`id_depense`),
-  ADD KEY `id_depense` (`id_depense`);
-
---
--- Index pour la table `depense`
---
-ALTER TABLE `depense`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_payeur` (`id_payeur`);
-
---
--- Index pour la table `membre`
---
-ALTER TABLE `membre`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_tricount` (`id_tricount`);
-
---
--- Index pour la table `tricount`
---
-ALTER TABLE `tricount`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `depense`
---
-ALTER TABLE `depense`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `membre`
---
-ALTER TABLE `membre`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `tricount`
---
-ALTER TABLE `tricount`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `concerner`
---
-ALTER TABLE `concerner`
-  ADD CONSTRAINT `concerner_ibfk_1` FOREIGN KEY (`id_concerne`) REFERENCES `membre` (`id`),
-  ADD CONSTRAINT `concerner_ibfk_2` FOREIGN KEY (`id_depense`) REFERENCES `depense` (`id`);
-
---
--- Contraintes pour la table `depense`
---
-ALTER TABLE `depense`
-  ADD CONSTRAINT `depense_ibfk_1` FOREIGN KEY (`id_payeur`) REFERENCES `membre` (`id`);
-
---
--- Contraintes pour la table `membre`
---
-ALTER TABLE `membre`
-  ADD CONSTRAINT `membre_ibfk_1` FOREIGN KEY (`id_tricount`) REFERENCES `tricount` (`id`);
+DROP TABLE IF EXISTS `tricount`;
+CREATE TABLE IF NOT EXISTS `tricount` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
